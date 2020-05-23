3030a6fac007431983234f06f2eade7c3f64e37c
IgorTheLight <igorthelight@gmail.com>
2020-05-23 20:18:34 +0300

    Fixed Panel resizing bug

--- DoW Mod Manager/ModManagerForm.Designer.cs
+++ DoW Mod Manager/ModManagerForm.Designer.cs
@@ -273,7 +273,6 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.AutoSize = true;
             this.panel1.Controls.Add(this.splitContainer1);
             this.panel1.Location = new System.Drawing.Point(12, 131);
             this.panel1.Name = "panel1";
