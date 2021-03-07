75c1fb401d204bab848a62c6064071823dc6a09a
IgorTheLight <igorthelight@gmail.com>
2021-03-07 20:40:45 +0200

    FogRemover.cs - even more human-friendlier

--- DoW Mod Manager/FogRemover.cs
+++ DoW Mod Manager/FogRemover.cs
@@ -4,8 +4,7 @@
 // MVID: ABA6371C-50C2-412D-8152-53D605E42626
 // Assembly location: D:\THQ\Dawn of War - Soulstorm\SSNoFog.exe
 // This was decompiled from the "SSNoFog.exe" made by the russian DoW modding community.
-
-// This file was changed by IgorTheLight to be more human-friendly
+// And later this file was changed by IgorTheLight to be more human-friendly
 
 using System;
 using System.Collections.Generic;
@@ -23,10 +22,11 @@ namespace SSNoFog
         private static readonly byte[] jmpFog            = new byte[6] { 217, 129,  96,  12,   0,   0 };
         private static readonly byte[] jmpMapSkyDistance = new byte[6] { 217, 155, 112,  12,   0,   0 };
         
-        private const int PROCESS_VM_OPERATION   =  8;
-        private const int PROCESS_WM_READ        = 16;
-        private const int PROCESS_WM_WRITE       = 32;
-        private const int PAGE_EXECUTE_READWRITE = 64;
+        private const int PROCESS_VM_OPERATION   = 0x0008;
+        private const int PROCESS_WM_READ        = 0x0010;
+        private const int PROCESS_WM_WRITE       = 0x0020;
+
+        private const int PAGE_EXECUTE_READWRITE = 0x40;
 
         private const int fogAddress12               = 0x745570;
         private const int float512Address12          = 0x863B18;
@@ -53,11 +53,11 @@ namespace SSNoFog
 
         public static void DisableFog(Process process)
         {
-            DisableFog(process, fogAddress12, float512Address12, mapSkyDistanceAddress12);
-            DisableFog(process, fogAddressSteam, float512AddressSteam, mapSkyDistanceAddressSteam);
+            ActuallyDisableFog(process, fogAddress12, float512Address12, mapSkyDistanceAddress12);
+            ActuallyDisableFog(process, fogAddressSteam, float512AddressSteam, mapSkyDistanceAddressSteam);
         }
 
-        private static void DisableFog(Process process, int fogAddress, int float512Address, int mapSkyDistanceAddress)
+        private static void ActuallyDisableFog(Process process, int fogAddress, int float512Address, int mapSkyDistanceAddress)
         {
             IntPtr pHandle = OpenProcess(56, false, process.Id);
             try
@@ -76,10 +76,12 @@ namespace SSNoFog
         {
             byte[] lpBuffer = new byte[checkVal.Length];
 
-            if (!ReadProcessMemory(pHandle, addr, lpBuffer, lpBuffer.Length, out int lpNumberOfBytesRead) || lpNumberOfBytesRead != lpBuffer.Length || !((IEnumerable<byte>)lpBuffer).SequenceEqual(checkVal))
+            if (!ReadProcessMemory(pHandle, addr, lpBuffer, lpBuffer.Length, out int lpNumberOfBytesRead)
+                || lpNumberOfBytesRead != lpBuffer.Length
+                || !((IEnumerable<byte>)lpBuffer).SequenceEqual(checkVal))
                 return false;
 
-            VirtualProtectEx(pHandle, addr, setVal.Length, 64, out int lpflOldProtect);
+            VirtualProtectEx(pHandle, addr, setVal.Length, PAGE_EXECUTE_READWRITE, out int lpflOldProtect);
             int returnCode = WriteProcessMemory(pHandle, addr, setVal, setVal.Length, out _) ? 1 : 0;
             
             VirtualProtectEx(pHandle, addr, setVal.Length, lpflOldProtect, out _);
