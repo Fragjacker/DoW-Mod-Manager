6f069c909abcd19a819e3b6d27a6f03f9367823d
AndO3131 <andre.owsiejczuk@wp.pl>
2022-08-11 13:06:10 +0200

    One last "detour" reference removed.

--- UNI_EXT/dllmain.cpp
+++ UNI_EXT/dllmain.cpp
@@ -2,34 +2,6 @@
 #include "stdafx.h"
 #include "UNI_EXT.h"
 
-BOOL WINAPI DetourCreateProcessWithDllEx();
-/*
-BOOL WINAPI DetourCreateProcessWithDllExA(_In_opt_ LPCSTR lpApplicationName,
-	_Inout_opt_ LPSTR lpCommandLine,
-	_In_opt_ LPSECURITY_ATTRIBUTES lpProcessAttributes,
-	_In_opt_ LPSECURITY_ATTRIBUTES lpThreadAttributes,
-	_In_ BOOL bInheritHandles,
-	_In_ DWORD dwCreationFlags,
-	_In_opt_ LPVOID lpEnvironment,
-	_In_opt_ LPCSTR lpCurrentDirectory,
-	_In_ LPSTARTUPINFOA lpStartupInfo,
-	_Out_ LPPROCESS_INFORMATION lpProcessInformation,
-	_In_ LPCSTR lpDllName,
-	_In_opt_ PDETOUR_CREATE_PROCESS_ROUTINEA pfCreateProcessA);
-
-BOOL WINAPI DetourCreateProcessWithDllExW(_In_opt_ LPCWSTR lpApplicationName,
-	_Inout_opt_  LPWSTR lpCommandLine,
-	_In_opt_ LPSECURITY_ATTRIBUTES lpProcessAttributes,
-	_In_opt_ LPSECURITY_ATTRIBUTES lpThreadAttributes,
-	_In_ BOOL bInheritHandles,
-	_In_ DWORD dwCreationFlags,
-	_In_opt_ LPVOID lpEnvironment,
-	_In_opt_ LPCWSTR lpCurrentDirectory,
-	_In_ LPSTARTUPINFOW lpStartupInfo,
-	_Out_ LPPROCESS_INFORMATION lpProcessInformation,
-	_In_ LPCSTR lpDllName,
-	_In_opt_ PDETOUR_CREATE_PROCESS_ROUTINEW pfCreateProcessW);
-*/
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
